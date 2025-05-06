a5c645e2bc3fa7b280169d8773099bfe8140e741
김승우 <rnqhqaltjs@naver.com>
2025-05-07 00:49:30 +0900

    Initial commit

---
+++ src/main/java/com/example/chatboard/ChatboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatboard/chatboard/ChatboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatboard.chatboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
